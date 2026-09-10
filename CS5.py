1efff45aabdbd53c1576c4c1b863d4702981fa18
rmlakesh0702-crypto <rmlakesh0702@gmail.com>
2026-09-10 12:54:33 +0530

    Area of rectangle

---
+++ CS5.py
@@ -0,0 +1,4 @@
+Length=int(input('Enter Length:'))
+Breadth=int(input('Enter Breadth:'))
+Areaofrectangle=Length*Breadth
+print('Area of Rectangle:',Areaofrectangle)
---
+++ CS6.py
@@ -0,0 +1,3 @@
+Temp_celsius=float(input('Enter Temperature in celsius:'))
+F=(Temp_celsius*9/5)+32
+print('Temperature in Fahrenheit:',F)
